05190670a81b7cd374dae58945b2c434c62fd236
Claudius Kastner <claukast@gmail.com>
2020-09-19 17:37:03 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tgenv",
-    version="1.0.0",
+    version="1.0.1",
     install_requires=["click", "requests", "tqdm", "PyGithub"],
     description="A tool for managing terragrunt versions",
     url="https://gitlab.com/claudiuskastner/tgenv",
