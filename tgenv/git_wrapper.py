e18990c0354cacddbcd24143661b09405c314ce1
Claudius Kastner <claukast@gmail.com>
2020-09-22 20:45:49 +0000

    Development

--- tgenv/git_wrapper.py
+++ tgenv/git_wrapper.py
@@ -11,7 +11,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with tgenv.  If not, see <https://www.gnu.org/licenses/>."""
+    along with tgenv.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 from github import Github
 
